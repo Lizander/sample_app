013a67237f0f37c3c73acf96b5176e0321e94003
Kevin Lizander <kevin9075@gmail.com>
2015-06-26 22:51:16 -0400

    Finishe log in/log out

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,19 +1,19 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
-  
+
   test "invalid singup information" do
 	get signup_path
 	assert_no_difference 'User.count' do
 	  	post users_path, user: { name: "",
 	  							 email: "user@invalid",
 	  							 password:              "foo",
-	  							 password_confirmation: "bar" } 
+	  							 password_confirmation: "bar" }
 	end
-	assert_template 'users/new'	
+	assert_template 'users/new'
   end
 
-  test "valid signup information" do 
+  test "valid signup information" do
   	get signup_path
   	assert_difference 'User.count', 1 do
   		post_via_redirect users_path, user: { name: "Example User",
@@ -22,5 +22,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   											  password_confirmation: "password"}
   	end
   	assert_template 'users/show'
+    assert is_logged_in?
   end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,7 +10,7 @@ class UserTest < ActiveSupport::TestCase
 		assert @user.valid?
 	end
 
-	test "name should be present" do 
+	test "name should be present" do
 		@user.name = "    "
 		assert_not @user.valid?
 	end
@@ -31,7 +31,7 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "email validation should accept valid addresses" do
-		valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org 
+		valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
 							first.last@foo.jp alice+bob@baz.cn]
 		valid_addresses.each do |valid_address|
 			@user.email = valid_address
@@ -60,8 +60,12 @@ class UserTest < ActiveSupport::TestCase
 		assert_not @user.valid?
 	end
 
-	test "password should have a minimum lenght" do 
+	test "password should have a minimum length" do
 		@user.password = @user.password_confirmation = "a" * 5
 		assert_not @user.valid?
+	end
+
+	test "authenticated? should return false for a user with nil digest" do
+		assert_not @user.authenticated?('')
 	end
 end
